7f040fd503ea4b97b3ecb131117fc2a738077eba
Alexis Cano <canoalexis12@gmail.com>
2023-09-02 15:17:41 -0600

    Adding images and css

--- test.js
+++ test.js
@@ -1,29 +1,75 @@
-function idioma()
-{
-    alert("Seleccione el idioma");
+let pagina = 1;
+        let idiomaSeleccionado = 'es-MX'; // Idioma predeterminado
 
-}
+        const btnAnterior = document.getElementById('btnAnterior');
+        const btnSiguiente = document.getElementById('btnSiguiente');
+        const btnEspanol = document.getElementById('btnEspanol'); // Botón para español
+        const btnIngles = document.getElementById('btnIngles'); // Botón para inglés
+        const txtTitulo = document.getElementById('txtTitulo');
 
-function test()
-{
-    const language = 'es';
-    const options = 
-{
-    method: 'GET',
-    headers: 
-    {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjAxMTM4YTdiN2FkNzIxODFjNGUzYmU1ZjBkZTgxMyIsInN1YiI6IjY0ZWE4YThjYzVjMWVmMDEwMjg2NTdkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KXmA5AijIGvq3HXllzl-rq94hMfs_m7NHLHwZAiabqc'
-    }
+        btnSiguiente.addEventListener('click', () => {
+            if (pagina < 1000) {
+                pagina += 1;
+                cargarPeliculas();
+            }
+        });
 
-};
+        btnAnterior.addEventListener('click', () => {
+            if (pagina > 1) {
+                pagina -= 1;
+                cargarPeliculas();
+            }
+        });
 
-fetch(`https://api.themoviedb.org/3/genre/movie/list?language=${language}`, options )
-.then(response => response.json())
-.then(responseJson => console.log(responseJson))
-.catch(err => console.error(err));
+        btnEspanol.addEventListener('click', () => {
+            idiomaSeleccionado = 'es-MX';
+            cargarPeliculas();
+            txtTitulo.textContent = "Peliculas Populares"
+            btnAnterior.textContent = "Anterior"
+            btnSiguiente.textContent = "Siguiente"
+        });
 
+        btnIngles.addEventListener('click', () => {
+            idiomaSeleccionado = 'en-US'; // Cambiar a 'en-US' para inglés
+            cargarPeliculas();
+            txtTitulo.textContent = "Most Popular Movies"
+            btnAnterior.textContent = "Previous"
+            btnSiguiente.textContent = "Next"
+        });
 
+        const cargarPeliculas = async () => {
+            try {
+                const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=${idiomaSeleccionado}&page=${pagina}`);
 
-    
-}
+                console.log(respuesta);
+
+                // Si la respuesta es correcta
+                if (respuesta.status === 200) {
+                    const datos = await respuesta.json();
+
+                    let peliculas = '';
+                    datos.results.forEach(pelicula => {
+                        peliculas += `
+                            <div class="pelicula">
+                                <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                                <h3 class="titulo">${pelicula.title}</h3>
+                            </div>
+                        `;
+                    });
+
+                    document.getElementById('contenedor').innerHTML = peliculas;
+
+                } else if (respuesta.status === 401) {
+                    console.log('Pusiste la llave mal');
+                } else if (respuesta.status === 404) {
+                    console.log('La película que buscas no existe');
+                } else {
+                    console.log('Hubo un error y no sabemos qué pasó');
+                }
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        cargarPeliculas();
